a1f11ce3645ad648b526de84165b2ea5a8298180
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-05 09:54:17 +0530

    done

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? 'https://ak-sketchbook.onrender.com' : 'http://localhost:5000'
+const URL = process.env.NODE_ENV === 'production' ? 'https://online-sketchbook-ak.vercel.app/' : 'http://localhost:5000'
 export const socket = io(URL);
